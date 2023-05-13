184d9c4c18191d6aa727549541371858b1a80c83
Paboladi24 <rapha@192.168.1.30>
2023-05-13 13:32:43 +0200

    snail procedure modifs

--- src/main/java/net/mcreator/smpmodv/procedures/SnaildensignprocProcedure.java
+++ src/main/java/net/mcreator/smpmodv/procedures/SnaildensignprocProcedure.java
@@ -5,13 +5,23 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.TickEvent;
 
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.core.BlockPos;
 
 import net.mcreator.smpmodv.init.SmpModVModBlocks;
+import net.mcreator.smpmodv.entity.LushgoblinEntity;
 
 import javax.annotation.Nullable;
+
+import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Comparator;
 
 @Mod.EventBusSubscriber
 public class SnaildensignprocProcedure {
@@ -31,6 +41,19 @@ public class SnaildensignprocProcedure {
 			return;
 		if ((world.getBlockState(new BlockPos(x, y + 2, z))).getBlock() == SmpModVModBlocks.SNAILDENSIGN.get()) {
 			entity.getPersistentData().putBoolean("arrived4", (true));
+			{
+				final Vec3 _center = new Vec3(x, y, z);
+				List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(1000 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center)))
+						.collect(Collectors.toList());
+				for (Entity entityiterator : _entfound) {
+					if (entityiterator instanceof LushgoblinEntity) {
+						if (entityiterator instanceof TamableAnimal _tamIsTamedBy && entity instanceof LivingEntity _livEnt ? _tamIsTamedBy.isOwnedBy(_livEnt) : false) {
+							if (entityiterator instanceof TamableAnimal _toTame && entityiterator instanceof Player _owner)
+								_toTame.tame(_owner);
+						}
+					}
+				}
+			}
 		}
 	}
 }
