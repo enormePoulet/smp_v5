ce49f0f149e9a25e5ea692c536ddd7c22f095f3e
enormePoulet <gabriel.falcou@gmail.com>
2023-05-13 09:34:08 +0200

    base commit

---
+++ src/main/java/net/mcreator/smpmodv/procedures/SnaildensignprocProcedure.java
@@ -0,0 +1,36 @@
+package net.mcreator.smpmodv.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.TickEvent;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.core.BlockPos;
+
+import net.mcreator.smpmodv.init.SmpModVModBlocks;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class SnaildensignprocProcedure {
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase == TickEvent.Phase.END) {
+			execute(event, event.player.level, event.player.getX(), event.player.getY(), event.player.getZ(), event.player);
+		}
+	}
+
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		execute(null, world, x, y, z, entity);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		if ((world.getBlockState(new BlockPos(x, y + 2, z))).getBlock() == SmpModVModBlocks.SNAILDENSIGN.get()) {
+			entity.getPersistentData().putBoolean("arrived4", (true));
+		}
+	}
+}
